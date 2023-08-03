3be0c2740cdad3d85f23b6bfc78b01cc2cc934fe
Ailken <ailkencf@gmail.com>
2023-08-03 14:45:43 -0300

    exercicios de Objetos exercicio 05(Cadastro de aulas)

---
+++ 05/cadastroDeAulas.js
@@ -0,0 +1,27 @@
+const aulas = [
+    {
+        Identificador: 1,
+        Nome_da_aula: "Introdução a programação"
+    },
+    {
+        Identificador: 2,
+        Nome_da_aula: "Variáveis"
+    },
+    {
+        Identificador: 3,
+        Nome_da_aula: "Condicionais"
+    },
+    {
+        Identificador: 4,
+        Nome_da_aula: "Arrays"
+    },
+];
+
+
+const curso = {
+    id: 1234,
+    nome: "Lógica de programação",
+    aulas
+}
+
+console.log(curso);
