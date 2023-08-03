edfcebe1379de8e1200df72636c40638df750394
Ailken <ailkencf@gmail.com>
2023-08-03 14:00:38 -0300

    exercicios de Objetos (exercicio 02)

---
+++ 02/listaDeVeiculos.js
@@ -0,0 +1,27 @@
+const carro1 =
+{
+    marca: "wolksvagem",
+    modelo: "GOL G6",
+    ano: 2014,
+    cor: "prata",
+    quantidade_portas: 4,
+    automatico: false
+};
+const carro2 = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    ano: 2020,
+    cor: "preto",
+    quantidade_portas: 4,
+    automatico: true
+};
+const carro3 = {
+    marca: "Honda",
+    modelo: "Civic G10",
+    ano: 2022,
+    cor: "Cinza",
+    quantidade_portas: 4,
+    automatico: true
+};
+
+console.log(carro3);
