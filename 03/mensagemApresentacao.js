d75c74925426a3a6f018ab4c19e05af6472b26b5
Ailken <ailkencf@gmail.com>
2023-08-03 14:05:25 -0300

    exercicios de Objetos (exercicio 03)

---
+++ 03/mensagemApresentacao.js
@@ -0,0 +1,9 @@
+const usuario = {
+    nome: "Kemuel",
+    idade: 20,
+    profissao: "Médico",
+    cor_preferida: "verde",
+}
+
+console.log(`Olá... meu nome é ${usuario.nome} e tenho ${usuario.idade} anos de idade.
+Sou ${usuario.profissao} e minha cor preferida é ${usuario.cor_preferida}`);
