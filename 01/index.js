810e77662fded9e7b57f1e2de20db995bb8093ce
Ailken <ailkencf@gmail.com>
2023-08-03 10:26:32 -0300

    exercicios de Objetos (exercicio 01)

---
+++ 01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: "Rua da alegria",
+    numero: 147,
+    complemento: "casa 01",
+    bairro: "Felicidade",
+    cep: 5555555,
+    cidade: "Recife",
+    estado: "PE",
+    pais: "Brasil"
+}
+
+const usuario = {
+    nome: "Ailken",
+    email: "asasa@gmail.com",
+    telefone: "(81) 99999-9999",
+    data_nascimento: "00/00/0000",
+    endereco
+}
+
+console.log(usuario)
